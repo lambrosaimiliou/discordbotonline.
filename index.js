8492082b1ab1fb4245e740230f7d559abe320f20
lambrosaimiliou <137066768+lambrosaimiliou@users.noreply.github.com>
2023-12-15 23:34:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const Discord = require('discord.js')
+const client = Discord.Client()
+const token = ("your token")
 
+client.on('ready', () => {
+  console.log("bot is ready")
+});
+
+client.login('token')
